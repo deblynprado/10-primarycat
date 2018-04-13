97ca4f8142c2619e0ec6e2ccc51d8c4fff21e839
Deblyn Prado <deblynprado@hotmail.com>
2018-04-13 16:00:18 -0300

    Creating a list on frontend to show posts

--- classes/10-primarycat-widget.php
+++ classes/10-primarycat-widget.php
@@ -15,21 +15,32 @@ class pricat_widget extends WP_Widget {
     );
   }
 
-// Creating widget front-end
   public function widget( $args, $instance ) {
     $title = apply_filters( 'widget_title', $instance['title'] );
 
-// before and after widget arguments are defined by themes
     echo $args['before_widget'];
     if ( ! empty( $title ) )
       echo $args['before_title'] . $title . $args['after_title'];
 
-// This is where you run the code and display the output
-    echo __( 'Hello, World!', '10pricat' );
+    $query_args = array(
+      'meta_key'   => 'pricat_get_categories',
+      'meta_value' => $instance["select"]
+    );
+    $the_query = new WP_Query( $query_args );
+
+    if ( $the_query->have_posts() ) {
+      echo '<ul class="primary-category-list category-' . $instance["select"] .'">';
+      while ( $the_query->have_posts() ) {
+        $the_query->the_post();
+        printf( '<li><a href="%1$s">%2$s</a></li>', get_the_permalink(), get_the_title() );
+      }
+      echo '</ul>';
+    } else {
+    }
+    wp_reset_postdata();
     echo $args['after_widget'];
   }
 
-// Widget Backend
   public function form( $instance ) {
     if ( isset( $instance[ 'title' ] ) ) {
       $title = $instance[ 'title' ];
@@ -42,26 +53,26 @@ class pricat_widget extends WP_Widget {
       $select = $instance['select'];
     else:
       $select = "";
-    endif; ?>
+      endif; ?>
 
-    <p>
-      <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-    </p>
-    <p>
-      <select name="<?php echo $this->get_field_name( 'select' ); ?>" id="<?php echo $this->get_field_id( 'select' ); ?>">
-        <?php $primary_posts = get_all_primary(); ?>
-        <?php foreach ( $primary_posts as $key => $pp ) {
-          $category = get_category_by_slug( $pp['primary'] );
+      <p>
+        <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+      </p>
+      <p>
+        <select name="<?php echo $this->get_field_name( 'select' ); ?>" id="<?php echo $this->get_field_id( 'select' ); ?>">
+          <?php $primary_posts = get_all_primary(); ?>
+          <?php foreach ( $primary_posts as $key => $pp ) {
+            $category = get_category_by_slug( $pp['primary'] );
+            ?>
+            <option value="<?php echo $pp['primary']; ?>" <?php selected( $select, $pp['primary'], true ); ?>>
+              <?php echo $category->name; ?>
+
+            </option>
+            <?php
+          }
           ?>
-          <option value="<?php echo $pp['primary']; ?>" <?php selected( $select, $pp['primary'], true ); ?>>
-            <?php echo $category->name; ?>
-
-          </option>
-        <?php
-      }
-       ?>
-      </select>
+        </select>
       </p>
       <?php
     }
